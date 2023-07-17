6b122f83b2e543ab083a662ffefdb1bd28c372b9
eniabellver <eugeniabellver@gmail.com>
2023-07-17 16:31:05 +0100

    csv is ingested properly

--- tf-model.js
+++ tf-model.js
@@ -1,42 +1,49 @@
 const tf = require('@tensorflow/tfjs-node');
 const csv = require('csv-parser');
 const fs = require('fs');
+const natural = require('natural');
 
-const csvFilePath = './files/SMSSpamCollection.csv';
+const csvFilePath = './files/sms_spam_dataset.csv';
 
 const textData = [];
 const labelData = [];
 
 fs.createReadStream(csvFilePath)
-  .pipe(csv())
+  .pipe(csv({skipLines: 1})) //skip header line
   .on('data', (row) => {
-    textData.push(row.text);
+    textData.push(row.sms);
     labelData.push(row.label);
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
 
-    // convert data to tensors
-    const textTensor = tf.tensor2d(textData, [textData.length, 1]);
-    const labelTensor = tf.tensor2d(labelData, [labelData.length, 1]);
+    // label numeric vals: 0 = ham, 1 = spam
+    const numericLabelData = labelData.map((label) => parseInt(label));
 
-    // normalize text tensor
-    const normalizedTextTensor = textTensor.div(tf.scalar(255));
+    // arrange text data
+    
 
-    // create and compile the model
+    // normalise
+    const normalizedTextData = dataset.map((text) => text.div(tf.scalar(255)));
+    const labelTensor = tf.tensor1d(numericLabelData);
+
+    // create and compile
     const model = tf.sequential();
     model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
     model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 
-    // train the model
-    model
-      .fit(normalizedTextTensor, labelTensor, {
-        epochs: 10,
-        batchSize: 32,
-        callbacks: tf.node.tensorBoard('logs'),
+    // train
+    normalizedTextData
+      .forEachAsync((text) => {
+        const textTensor = tf.tensor2d([text.dataSync()], [1, 1]);
+        return model.fit(textTensor, labelTensor, {
+          epochs: 10,
+          batchSize: 32,
+          callbacks: tf.node.tensorBoard('logs'),
+        });
       })
-      .then((history) => {
-        const prediction = model.predict(normalizedTextTensor);
+      .then(() => {
+        const prediction = model.predict(normalizedTextData);
 
         console.log('Prediction:', prediction);
       });
